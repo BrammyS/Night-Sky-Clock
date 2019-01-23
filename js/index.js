54e0cb8edd6ce1e148c6ddb00ee21f2f4df3b06a
BramEsendam <bram.esendam@gmail.com>
2019-01-23 22:33:42 +0100

    Added time logging to console

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-﻿
+﻿var columns = Array.from(document.getElementsByClassName('column'));
+var numHeight = 50;
+
+window.onload = startTime();
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ms = today.getMilliseconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkTime(ms);
+    console.log(h + ":" + m + ":" + s + ":" + ms);
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    // add zero in front of numbers < 10
+    if (i < 10) {
+        i = "0" + i;
+    } 
+    return i;
+}
