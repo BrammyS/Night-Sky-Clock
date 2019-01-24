2e9954b3c14bdf0e67c707f33c62792fbe659d1f
Bram Esendam <bram.esendam@gmail.com>
2019-01-24 11:14:01 +0100

    Added correct time to clock

--- js/index.js
+++ js/index.js
@@ -1,25 +1,60 @@
-﻿var columns = Array.from(document.getElementsByClassName('column'));
-var numHeight = 50;
-var h = 0,
-    m = 0,
-    s = 0;
+﻿
+var columns = Array.from(document.getElementsByClassName('column'));
+var numHeight = 100;
 
-window.onload = startTime();
+var Time =
+[
+    0, 0, // Hours
+    0, 0, // Minutes
+    0, 0  // Seconds
+];
 
-function startTime() {
-    var today = new Date();
-    h = today.getHours();
-    m = today.getMinutes();
-    s = today.getSeconds();
-    console.log(h + ":" + m + ":" + s);
+
+function StartTime() {
+
+    var tempTime = GetDate();
+
+    // Loop through hours, minutes, seconds.
+    for (var i = 0; i < tempTime.length; i++) {
+
+        // If hours, minutes or seconds is less then 10, add a 0 to the first digit.
+        if (tempTime[i] < 10) {
+            Time[i + i] = 0;
+            Time[i + i + 1] = tempTime[i];
+        } else {
+            Time[i + i] = parseInt(tempTime[i].toString().split('')[0]);
+            Time[i + i + 1] = parseInt(tempTime[i].toString().split('').pop());
+        }
+    }
+    console.log(Time[0] + "" + Time[1] + ":" + Time[2] + "" + Time[3] + ":" + Time[4] + "" + Time[5]);
+
     MoveRow();
-    var t = setTimeout(startTime, 500);
+    setTimeout(StartTime, 500);
 }
 
+
+// Returns a array of hours, minutes, seconds.
+function GetDate() {
+    var newTime = new Date();
+    return [newTime.getHours(), newTime.getMinutes(), newTime.getSeconds()];
+}
+
+
+// Moves the rows to the correct position.
 function MoveRow() {
     columns.forEach(function (element, i) {
-        var offset = -parseInt(s.toString().split('').pop()) * numHeight;
-        element.style.transform = 'translateY(calc(50vh + ' + offset + 'px - ' + numHeight / 2 + 'px))';
+            var offset = -Time[i] * numHeight;
+            element.style.transform = 'translateY(calc(50vh + ' + offset + 'px - ' + numHeight + 'px))';
+            columns.forEach(function (element, i) {
+
+                }
+            );
         }
     );
 }
+
+
+// Start timer when the page loads.
+window.onload = StartTime();
+
+
